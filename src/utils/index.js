91ace5b7b5dc78750a494f26218f9ebdc83d3336
Luis Miguel Alvarado <--global>
2021-03-14 14:05:49 -0400

    wip

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,8 +2,3 @@ export const LOGO = {
   uri:
     'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/717px-NASA_logo.svg.png',
 };
-
-export const BACKGROUND_IMAGE = {
-  uri:
-    'https://imagevars.gulfnews.com/2018/12/17/Mars-planet-(Read-Only)_resources1_16a085441a6_large.jpg',
-};
