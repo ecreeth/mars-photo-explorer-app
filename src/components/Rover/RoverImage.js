556526e07bcce2c64c2f53e312fec743039be8dd
Luis Miguel Alvarado <--global>
2021-03-14 20:22:55 -0400

    wip

--- src/components/Rover/RoverImage.js
+++ src/components/Rover/RoverImage.js
@@ -7,8 +7,7 @@ const ROBERT_NAME = {
   Curiosity: `${URL}/Curiosity_rover.jpg`,
   Spirit: `${URL}/Spirit_rover.jpg`,
   Opportunity: `${URL}/Opportunity_rover.jpg`,
-  Perseverance:
-    'https://www.techrepublic.com/a/hub/i/r/2020/07/16/17c3d680-eb3f-45c0-a0d6-aebfcf816133/thumbnail/770x578/6aa7318b94e944d91d45376388c22981/perseverance-rover.jpg',
+  Perseverance: `${URL}/Perseverance_rover.jpg`,
 };
 
 const RoverImage = ({name}) => (
